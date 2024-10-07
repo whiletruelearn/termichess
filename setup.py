55987c3dd51bd9fbb01ededa7dc8c82afbb6845f
whiletruelearn <kskrishnasangeeth@gmail.com>
2024-10-07 21:25:27 +0530

    update the manifest and change the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="termichess",
-    version="0.1.1",
+    version="0.1.2",
     author="Krishna Sangeeth KS",
     author_email="kskrishnasangeeth@gmail.com",
     description="A chess game on your terminal",
