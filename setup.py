af6064489eac6f7be6d6799ed1b2a38afb35c108
whiletruelearn <kskrishnasangeeth@gmail.com>
2024-10-07 21:22:17 +0530

    Bump up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="termichess",
-    version="0.1.0",
+    version="0.1.1",
     author="Krishna Sangeeth KS",
     author_email="kskrishnasangeeth@gmail.com",
     description="A chess game on your terminal",
