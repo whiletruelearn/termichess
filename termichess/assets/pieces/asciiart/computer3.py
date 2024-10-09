3b87fa079be623cbb14ef95769714fe3d01fb53e
whiletruelearn <kskrishnasangeeth@gmail.com>
2024-10-09 18:10:36 +0530

    Add ASCII based pieces as they render bettr than pngs

---
+++ termichess/assets/pieces/asciiart/computer3.py
@@ -0,0 +1,73 @@
+PIECE_ASCII_ART = {
+    'P': """
+ 0
+/1\\
+▀▀▀
+""",  # Binary (pawn)
+
+    'N': """
+ λ
+/→\\
+▀▀▀
+""",  # Lambda function (knight)
+
+    'B': """
+ ∑
+<f>
+▀▀▀
+""",  # Algorithm/Sum (bishop)
+
+    'R': """
+[█]
+|S|
+▀▀▀
+""",  # Stack (rook)
+
+    'Q': """
+ ∞
+<O>
+▀▀▀
+""",  # Big O Notation (queen)
+
+    'K': """
+#!
+|C|
+▀▀▀
+""",  # Compiler/Shebang (king)
+
+    'p': """
+ 1
+/0\\
+▀▀▀
+""",  # Binary (pawn)
+
+    'n': """
+<>
+||
+▀▀▀
+""",  # Recursion (knight)
+
+    'b': """
+{T}
+/\\
+▀▀▀
+""",  # Tree data structure (bishop)
+
+    'r': """
+[Q]
+||
+▀▀▀
+""",  # Queue (rook)
+
+    'q': """
+@IP
+\\/
+▀▀▀
+""",  # Network/IP (queen)
+
+    'k': """
+ $
+/\\
+▀▀▀
+"""  # Shell (king)
+}
---
+++ termichess/assets/pieces/asciiart/mahabharat.py
@@ -0,0 +1,73 @@
+PIECE_ASCII_ART = {
+    'P': """
+ /\\
+/||\\
+▀▀▀▀
+""",  # Pandava soldier
+
+    'N': """
+<HH>
+ ][
+▀▀▀▀
+""",  # Horse (Ashwatthama)
+
+    'B': """
+{##}
+ ||
+▀▀▀▀
+""",  # Chakra (discus)
+
+    'R': """
+[II]
+|==|
+▀▀▀▀
+""",  # Indraprastha palace
+
+    'Q': """
+\\†/
+|D|
+▀▀▀▀
+""",  # Draupadi
+
+    'K': """
+ Ω
+/Y\\
+▀▀▀▀
+""",  # Yudhishthira (eldest Pandava)
+
+    'p': """
+ /\\
+/||\\
+▀▀▀▀
+""",  # Kaurava soldier
+
+    'n': """
+<HH>
+ ][
+▀▀▀▀
+""",  # Horse (Ashwatthama)
+
+    'b': """
+{##}
+ ||
+▀▀▀▀
+""",  # Chakra (discus)
+
+    'r': """
+[HH]
+|==|
+▀▀▀▀
+""",  # Hastinapur palace
+
+    'q': """
+\\†/
+|G|
+▀▀▀▀
+""",  # Gandhari
+
+    'k': """
+ Ω
+/D\\
+▀▀▀▀
+"""  # Duryodhana (eldest Kaurava)
+}
