9832bddd78c05b686a212afcf50f0453cead1437
whiletruelearn <kskrishnasangeeth@gmail.com>
2024-10-10 19:03:18 +0530

    Rename ascii art files

---
+++ termichess/assets/pieces/asciiart/computer_second.py
@@ -0,0 +1,73 @@
+PIECE_ASCII_ART = {
+   'P': """
+ 10
+/||\\
+▀▀▀▀
+""",  # Binary (pawn)
+
+    'N': """
+</>
+ R
+▀▀▀▀
+""",  # Recursion (knight)
+
+    'B': """
+ []
+/D\\
+▀▀▀▀
+""",  # Data Structure (bishop)
+
+    'R': """
+[AP]
+|I|
+▀▀▀▀
+""",  # API (rook)
+
+    'Q': """
+ @
+/N\\
+▀▀▀▀
+""",  # Network (queen)
+
+    'K': """
+ $
+/S\\
+▀▀▀▀
+""",  # Shell (king),
+
+    'p': """
+ 10
+/||\\
+▀▀▀▀
+""",  # Binary (pawn)
+
+    'n': """
+</>
+ R
+▀▀▀▀
+""",  # Recursion (knight)
+
+    'b': """
+ []
+/D\\
+▀▀▀▀
+""",  # Data Structure (bishop)
+
+    'r': """
+[AP]
+|I|
+▀▀▀▀
+""",  # API (rook)
+
+    'q': """
+ @
+/N\\
+▀▀▀▀
+""",  # Network (queen)
+
+    'k': """
+ $
+/S\\
+▀▀▀▀
+"""  # Shell (king)
+}
